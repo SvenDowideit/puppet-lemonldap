26fa1cd371433e7891209082a7af41ce45500b4a
Sven Dowideit <SvenDowideit@home.org.au>
2013-07-02 16:45:29 +1000

    lots of augeas hacking to set the domains

--- manifests/init.pp
+++ manifests/init.pp
@@ -34,7 +34,7 @@
 #   Puppet 2.6.)
 #
 # === Examples
-#
+# the simplest usage is to add the following to the intended auth server
 #  class { lemonldap:
 #    domain => 'fosiki.com'
 #  }
@@ -61,6 +61,7 @@ class lemonldap (   $domain='UNSET',
 
 #TODO: consider changing this to allow the user to over-ride the installation mech
 # eg $package = rpm, deb, tgz, cpan, git, rather than assuming based on OS
+#TODO: see http://jenkner.org/blog/2013/03/27/use-osfamily-instead-of-operatingsystem/ for facter < 1.6.1
   case $::osfamily {
     'redhat' : { $package = 'rpm' }
     default  : {  
@@ -76,6 +77,7 @@ class lemonldap (   $domain='UNSET',
     class { "lemonldap::${package}": }
     
     #add the built in vhostnames to the hosts file
+    #TODO: i wonder if we can export these to client boxes too
     host { "${authhost}.${domain}":
         ip => $ipaddress,
         host_aliases => [ "${authhost}"]
@@ -96,7 +98,111 @@ class lemonldap (   $domain='UNSET',
         ip => $ipaddress,
         host_aliases => [ "${test2host}"]
     }
+    
+    #TODO: work out how to allow the use of user specified templates - these are just the basic default
+    
+    #sed -i 's/example\.com/ow2.org/g' /etc/lemonldap-ng/* /var/lib/lemonldap-ng/conf/lmConf-1 /var/lib/lemonldap-ng/test/index.pl
+    #do the apache conf files using augeas - i'll need to find a better way to do this..
+    exec { "hack-lemonldap-conf":
+        command => "/bin/sed -i 's/example\.com/${domain}/g' /var/lib/lemonldap-ng/conf/lmConf-1 /var/lib/lemonldap-ng/test/index.pl",
+        #refreshonly => true,
+    }
+        
+    #now need to customise the /etc/httpd/conf.d/z-lemonldap* files to use the desired hostnames
+    file { "/etc/httpd/conf.d/z-lemonldap-ng-handler.conf": ensure => "absent" }
+    file { "/etc/httpd/conf.d/z-lemonldap-ng-manager.conf": ensure => "absent" }
+    file { "/etc/httpd/conf.d/z-lemonldap-ng-portal.conf": ensure => "absent" }
+    file { "/etc/httpd/conf.d/z-lemonldap-ng-test.conf": ensure => "absent" }
+
+    file { "/etc/httpd/conf.d/lemonldap-ng-handler.conf": ensure => "present", source => '/etc/lemonldap-ng/handler-apache2.conf' }
+    file { "/etc/httpd/conf.d/lemonldap-ng-manager.conf": ensure => "present", source => '/etc/lemonldap-ng/manager-apache2.conf' }
+    file { "/etc/httpd/conf.d/lemonldap-ng-portal.conf": ensure => "present", source => '/etc/lemonldap-ng/portal-apache2.conf' }
+    file { "/etc/httpd/conf.d/lemonldap-ng-test.conf": ensure => "present", source => '/etc/lemonldap-ng/test-apache2.conf' }
+
+    #trying to use augeas
+    #Package["ruby-augeas"] -> Augeas <| |>    
+
+    #TODO: need to uncomment a #NameVirtualHost *:80 (augeas isn't great at that)
+    file { "/etc/httpd/conf.d/name-virtual-host.conf": ensure => "present", content => 'NameVirtualHost *:80' }    
+    
+    augeas { "lemonldap-handler":
+      context => "/files/etc/httpd/conf.d/lemonldap-ng-handler.conf",
+      changes => [
+        "set VirtualHost/*[self::directive='ServerName']/arg ${reloadhost}.${domain}",
+        "set *[self::directive='ErrorDocument']/*[self::arg='http://auth.example.com/?lmError=403'] http://${authhost}.${domain}/?lmError=403",
+        "set *[self::directive='ErrorDocument']/*[self::arg='http://auth.example.com/?lmError=500'] http://${authhost}.${domain}/?lmError=500",
+        "set *[self::directive='ErrorDocument']/*[self::arg='http://auth.example.com/?lmError=503'] http://${authhost}.${domain}/?lmError=503",
+      ],
+      require => File["/etc/httpd/conf.d/lemonldap-ng-handler.conf"],
+    }    
+    augeas { "lemonldap-manager":
+      context => "/files/etc/httpd/conf.d/lemonldap-ng-manager.conf",
+      changes => [
+        "set VirtualHost/*[self::directive='ServerName']/arg ${managerhost}.${domain}",
+        "set *[self::directive='ErrorDocument']/*[self::arg='http://auth.example.com/?lmError=403'] http://${authhost}.${domain}/?lmError=403",
+        "set *[self::directive='ErrorDocument']/*[self::arg='http://auth.example.com/?lmError=500'] http://${authhost}.${domain}/?lmError=500",
+        "set *[self::directive='ErrorDocument']/*[self::arg='http://auth.example.com/?lmError=503'] http://${authhost}.${domain}/?lmError=503",
+      ],
+      require => File["/etc/httpd/conf.d/lemonldap-ng-manager.conf"],
+    }    
+    augeas { "lemonldap-portal":
+      context => "/files/etc/httpd/conf.d/lemonldap-ng-portal.conf",
+      changes => [
+        "set VirtualHost/*[self::directive='ServerName']/arg ${authhost}.${domain}",
+        "set *[self::directive='ErrorDocument']/*[self::arg='http://auth.example.com/?lmError=403'] http://${authhost}.${domain}/?lmError=403",
+        "set *[self::directive='ErrorDocument']/*[self::arg='http://auth.example.com/?lmError=500'] http://${authhost}.${domain}/?lmError=500",
+        "set *[self::directive='ErrorDocument']/*[self::arg='http://auth.example.com/?lmError=503'] http://${authhost}.${domain}/?lmError=503",
+      ],
+      require => File["/etc/httpd/conf.d/lemonldap-ng-portal.conf"],
+    }    
+    augeas { "lemonldap-test":
+      context => "/files/etc/httpd/conf.d/lemonldap-ng-test.conf",
+      changes => [
+        "set VirtualHost/*[self::directive='ServerName']/arg ${test1host}.${domain}",
+        "set VirtualHost/*[self::directive='ServerAlias']/arg ${test2host}.${domain}",
+        "set *[self::directive='ErrorDocument']/*[self::arg='http://auth.example.com/?lmError=403'] http://${authhost}.${domain}/?lmError=403",
+        "set *[self::directive='ErrorDocument']/*[self::arg='http://auth.example.com/?lmError=500'] http://${authhost}.${domain}/?lmError=500",
+        "set *[self::directive='ErrorDocument']/*[self::arg='http://auth.example.com/?lmError=503'] http://${authhost}.${domain}/?lmError=503",
+      ],
+      require => File["/etc/httpd/conf.d/lemonldap-ng-test.conf"],
+    }    
+    #TODO: need to bounce apache
+    
+    #open the firewall for the web
+    #https://forge.puppetlabs.com/arusso/iptables
+    #TODO: move out to be optional
+iptables::rule { 'allow web browsers':
+    comment          => 'let the web shine in',
+    priority         => '100',
+    protocol => 'tcp',    
+    destination_port => '80,443',
+    action            => 'ACCEPT',
+  }   
+    
+    #TODO: obviusly this needs to move out to be optional..
+    #https://forge.puppetlabs.com/spiette/selinux
+    #turn off selinux and the firewall
+    class { 'selinux':
+      mode => 'permissive'
+    }
+        
+   # We want to make sure that Apache is running.
+   service { "apache":
+      name => $::osfamily ? { redhat => 'httpd', default => 'apache'},
+      ensure => running,
+      hasstatus => true,
+      hasrestart => true,
+      #require => Package["apache2"],
+   }    
+    #restart apache!
+    exec { "reload-apache":
+        command => "/usr/sbin/apachectl graceful",
+        refreshonly => true,
+    }
   }
+  
+    
+  
 }
 
 
--- manifests/rpm.pp
+++ manifests/rpm.pp
@@ -12,4 +12,22 @@ class lemonldap::rpm {
     }
 
     package { "lemonldap-ng": ensure => "latest", require => Yumrepo["lemonldap"] }
+    
+    #TODO: maybe use an augeas puppet forge module?
+    #http://projects.puppetlabs.com/projects/1/wiki/puppet_augeas
+    #package { "augeas": ensure => "latest", require => Yumrepo["epel"] }
+    #package { "ruby-augeas": ensure => "latest", require => Yumrepo["epel"] }
+    
+    #TODO: only do if x64..
+    #http://lemonldap-ng.org/documentation/latest/installrpm
+    #
+    #If you install packages on 64bits system, create those symbolic links:
+    #ln -s /usr/lib/perl5/vendor_perl/5.8.8/Lemonldap /usr/lib64/perl5/
+    #ln -s /usr/lib/perl5/vendor_perl/5.8.8/auto/Lemonldap /usr/lib64/perl5/auto/
+    #restart apache!
+    exec { "lemonldap-x64":
+        command => "/bin/ln -s /usr/lib/perl5/vendor_perl/5.8.8/Lemonldap /usr/lib64/perl5/ ; /bin/ln -s /usr/lib/perl5/vendor_perl/5.8.8/auto/Lemonldap /usr/lib64/perl5/auto/",
+        refreshonly => true,
+    }
+    
 }
